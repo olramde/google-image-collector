b450bc05cf23951634c237477e177332e54d7816
olramde <rladhkstn8@gmail.com>
2019-08-27 00:19:38 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'google-image-collector'
+}
+</script>
+
+<style>
+body {
+  background-color: #282c34;
+}
+</style>
---
+++ src/renderer/components/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script>
+export default {
+  // TODO INPUT SEARCH COMPONENTS
+  // TODO LIST COMPONENTS
+  // TODO FOOTER COMPONENTS
+}
+</script>
+
+<style>
+</style>
